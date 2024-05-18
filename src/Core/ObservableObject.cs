83efbbfd9867223af2e5b6ec683f617a46eb9593
SandWithCheese <naufalahmad022@gmail.com>
2024-05-19 01:36:22 +0700

    init wpf project

---
+++ src/Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace src.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
